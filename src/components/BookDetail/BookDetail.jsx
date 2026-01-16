67dbf6f1739229c4eb87a1cf0d84799f83978e25
shamssaniat <shams.saniat@northsouth.edu>
2026-01-17 03:03:08 +0600

    Added book detail data

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const BookDetail = () => {
+
+    const { bookId } = useParams();
+    const data = useLoaderData();
+    const id = parseInt(bookId)
+    // console.log(typeof bookId, typeof id, typeof data[0].bookId );
+
+    const book = data.find(book => book.bookId === id);
+
+    const { bookId: currentBookId, image } = book;
+
     return (
         <div>
-            
+            <h2>Book details: {bookId}</h2>
+            <img className='w-36' src={image} alt="" />
+            <br />
+            <button className="btn btn-outline btn-accent">Read</button>
+            <button className="btn btn-accent">Wishlist</button>
+
         </div>
     );
 };
